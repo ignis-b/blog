4a522eb85276b88d72c81ad8d38639ced324e8ed
Igna Burmova <igna.nedyalkova@ffwagency.com>
2022-03-04 15:14:12 +0200

    Add new routes

--- app/routes.php
+++ app/routes.php
@@ -1,12 +1,26 @@
 <?php
-// Routes
+use App\Controllers\HomeAction;
+use App\Controllers\UserController;
+    use App\Controllers\UsersController;
 
 $app->get('/', App\Controllers\HomeAction::class)
     ->setName('homepage');
 
-$app->get('/users', function () {
-    $users = App\Models\Users::all();
-    echo $users->toJson();
-});
+$app->get('/users', UsersController::class);
 
+$app->get('/user/login', UserController::class);
+//$app->post('/user/login', [UserController::class, 'actionLogin']);
+
+$app->get('/myArticles', App\Controllers\HomeAction::class)
+ ->setName('myArticles');
+$app->get('/article_create', App\Controllers\HomeAction::class)
+ ->setName('article_create');
+$app->get('/user_profile', App\Controllers\HomeAction::class)
+ ->setName('user_profile');
+$app->get('/security_logout', App\Controllers\HomeAction::class)
+ ->setName('security_logout');
+$app->get('/security_login', App\Controllers\HomeAction::class)
+ ->setName('security_login');
+$app->get('/user_register', App\Controllers\HomeAction::class)
+ ->setName('user_register');
 $app->run();
