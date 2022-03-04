cddc955ebcd1e7761193fe9ca9be4e37d017d0fa
Igna Burmova <igna.nedyalkova@ffwagency.com>
2022-03-04 15:13:25 +0200

    Add Models/Users

--- app/src/Models/Users.php
+++ app/src/Models/Users.php
@@ -3,5 +3,13 @@ namespace App\Models;
 
 class Users extends \Illuminate\Database\Eloquent\Model
 {
-
+    public $table = 'user';
+    public $timestamps = false;
+    
+    protected $fillable = [
+     'emil',
+     'name',
+     'password_hash',
+     'role'
+    ];
 }
